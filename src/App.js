21e0690e9156bfebc2f89f555cf34c1983537a78
rudhir <rudhir97@gmail.com>
2021-06-27 04:43:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
-import { Table1 } from './components/Table1';
-import {Table2} from './components/Table2'
-
 function App() {
+  // const c=false;
+
   return (
     <div className="App">
-      {/* <Table2 /> */}
-      <Navbar/>
-      <Table1/>
+      <Navbar  />
+      
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
+import {Table2} from './Table2'
+import {Table1} from './Table1'
+
 
 const Navbar = () => {
+  const [c,setC]=useState(true);
+
+  const tab1=(c)=>{
+    setC(c=true)
+  }
+  const tab2=(c)=>{
+    setC(c=false)
+  }
   return (
     <div>
       <nav>
@@ -14,18 +25,19 @@ const Navbar = () => {
           </li>
 
           <li>
-            <a href="#about">
-              <span>Table1</span> <i class="far fa-address-card"></i>{" "}
+            <a href="#Table1">
+              <span onClick={tab1} >Table1</span> <i class="far fa-address-card"></i>{" "}
             </a>
           </li>
 
-          <li>
-            <a href="#skills">
-              <span>Table2</span> <i class="fas fa-laptop-code"></i>{" "}
+          <li >
+            <a href="#Table2">
+              <span onClick={tab2}>Table2</span> <i class="fas fa-laptop-code"></i>{" "}
             </a>
           </li>
         </ul>
       </nav>
+      {c?<Table1/>:<Table2/>}
     </div>
   );
 };
